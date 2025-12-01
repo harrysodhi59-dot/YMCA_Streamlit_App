f86bced1621850ff52a5d6d4a2a5144da30e99b6
Harasis Singh Sodhi <harrysodhi59@gmail.com>
2025-12-01 04:26:11 -0700

    Update 1_Data_Overview.py

--- ymca_app/pages/1_Data_Overview.py
+++ ymca_app/pages/1_Data_Overview.py
@@ -6,6 +6,10 @@ st.title("📄 Data Overview")
 @st.cache_data
 def load_data():
     return pd.read_csv("../data/ymca_clusters.csv")
+import os
+st.write("📌 Current Working Directory:", os.getcwd())
+st.write("📂 Files in current directory:", os.listdir())
+st.write("📂 Files in parent directory:", os.listdir(".."))
 
 df = load_data()
 
