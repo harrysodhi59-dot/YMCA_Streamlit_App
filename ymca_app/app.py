897db539f3ef1cbe4295dea7136eda5f9b6157cf
Harasis Singh Sodhi <harrysodhi59@gmail.com>
2025-12-03 23:30:01 -0700

    Update app.py

--- ymca_app/app.py
+++ ymca_app/app.py
@@ -1,14 +1,3 @@
-from PIL import Image
-import os
-
-logo_path = os.path.join(os.path.dirname(__file__), "assets", "ymca_logo.png")
-
-try:
-    logo = Image.open(logo_path)
-    st.sidebar.image(logo, use_column_width=True)
-except:
-    st.sidebar.write("")  # no crash if missing
-
 import streamlit as st
 
 # ----------------------------------------------------
